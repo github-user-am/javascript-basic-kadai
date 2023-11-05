62f4646caae1eed5de4c3bc64428d9608cea4d49
github-user-am <149214019+github-user-am@users.noreply.github.com>
2023-11-05 14:54:32 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+// 本日の年月日
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log(year + '年' + month + '月' + day + '日');
